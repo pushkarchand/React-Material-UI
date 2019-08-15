10f2dcc05a2f688622d6cb480646b4d6cbf02f6c
pushkarchand <31924186+pushkarchand@users.noreply.github.com>
2019-08-15 23:24:33 +0530

    Added signin and signup screen and added routing

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import "./styles.css";
-
+import SignUp from "./signup";
+import SignIn from "./signin";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Router>
+      <div>
+        <Route exact path="/" component={SignIn} />
+        <Route path="/signup" component={SignUp} />
+      </div>
+    </Router>
   );
 }
 
